791c5acb0ad7730fe8dc5c496a60d6bbd9584436
“alaa <alaaaliahmed2412@gmail.com>
2021-06-06 14:59:14 +0300

    test

---
+++ src/test/java/com/lucky/qa/steps/AddWrongOtpWhenVerifyingMobileNumberInCashoutTest.java
@@ -0,0 +1,31 @@
+package com.lucky.qa.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AddWrongOtpWhenVerifyingMobileNumberInCashoutTest {
+    @Given("portal opens in {string},user logged in")
+    public void portal_opens_in_user_logged_in(String string) {
+
+    }
+
+    @When("open wallet screen, click cashout button")
+    public void open_wallet_screen_click_cashout_button() {
+
+    }
+
+    @When("add {string} , click continue button")
+    public void add_click_continue_button(String string) {
+    }
+
+    @When("add wrong OTP then click continue")
+    public void add_wrong_otp_then_click_continue() {
+    }
+
+    @Then("error message displayed according to portal {string}")
+    public void error_message_displayed_according_to_portal(String string) {
+
+    }
+
+}
