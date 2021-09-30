2003c941165c7c8765abe48addc9db19b05a278c
“alaa <alaaaliahmed2412@gmail.com>
2021-09-30 11:44:30 +0300

    fix for translation files for jenkins and also changed some element locators

---
+++ test-output-thread/report.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "c0e93113-5c52-45ce-b191-377f9a01ca4b",
+    "feature": "Fields Validation During Registration",
+    "scenario": "user can not register with invalid values",
+    "start": 1632571095333,
+    "group": 14,
+    "content": "",
+    "tags": "@fieldsvalidationduringregistration,@moroccosmoketest,",
+    "end": 1632571223884,
+    "className": "failed"
+  },
+  {
+    "id": "c06d0de6-bcd0-49b3-ab84-7ea4991e5aa5",
+    "feature": "Fields Validation During Registration",
+    "scenario": "user can not register with invalid values",
+    "start": 1632571095334,
+    "group": 15,
+    "content": "",
+    "tags": "@fieldsvalidationduringregistration,@moroccosmoketest,",
+    "end": 1632571166302,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 14,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  },
+  {
+    "id": 15,
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  }
+]);
+});
